f0936d65dff514aa5c4abb172ca13708d97d5c36
maisaengineering <contact@maisasolutions.com>
2014-12-06 15:01:36 +0530

    fancybox and datepicker added

--- js/extra.js
+++ js/extra.js
@@ -15,3 +15,34 @@ $(document).ready(function() {
 
     
 });
+
+    $(document).ready(function() {
+      /*
+       *  Simple image gallery. Uses default settings
+       */
+
+      $('.fancybox').fancybox();
+
+      /*
+      /*
+       *  Media helper. Group items, disable animations, hide arrows, enable media and button helpers.
+      */
+      $('.fancybox-media')
+        .attr('rel', 'media-gallery')
+        .fancybox({
+          openEffect : 'none',
+          closeEffect : 'none',
+          prevEffect : 'none',
+          nextEffect : 'none',
+
+          arrows : false,
+          helpers : {
+            media : {},
+            buttons : {}
+          }
+        });
+
+      /*
+       *  Open manually
+       */
+    });
